5bc705e880dcae27b42d9f64f2419a917ecc91d5
shan <112062753+koze2totwo@users.noreply.github.com>
2025-05-09 12:55:29 +0530

    [128] [Time Beats: 60.38%] [Memory Beats: 66.73%] - LeetPush

---
+++ Longest-Consecutive-Sequence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> st(nums.begin(),nums.end());
+       
+        int c=0;
+        for(int x:st){
+            if(st.find(x-1)==st.end()){
+                int t=1;
+            while(st.find(++x)!= st.end()){
+                t++;
+            
+            }
+            c=max(c,t);
+            }
+            
+        }
+        return c;
+    }
+};
