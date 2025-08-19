3c3d8c0a0564b44fd78211247cd537132d30e764
shan <112062753+koze2totwo@users.noreply.github.com>
2025-08-19 21:25:34 +0530

    [2348] [Time Beats: 27.82%] [Memory Beats: 40.61%] - LeetPush

--- Number-of-Zero-Filled-Subarrays.cpp
+++ Number-of-Zero-Filled-Subarrays.cpp
@@ -3,7 +3,7 @@ public:
     long long zeroFilledSubarray(vector<int>& nums) {
         long long ans=0;
         int j=0;
-
+        //simple sliding window solution 
         for(int i = 0;i<nums.size();i++){
            j=i;
            int c=0;
@@ -12,7 +12,8 @@ public:
             }
             c=j-i;
             i=j;
-            ans=ans+ (long long)(((long long)c*(c+1))/2);
+            ans=ans+ (long long)(((long long)c*(c+1))/2); //used long to due to int exceeding value while multiplying 
+            //formula to add to ans for each subarrray
 
 
         }
