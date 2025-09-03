d932200d5ed2b838454b905d7d9ec677e29b1ba0
shan <112062753+koze2totwo@users.noreply.github.com>
2025-09-03 23:12:14 +0530

    [53] [Time Beats: 100.00%] [Memory Beats: 52.20%] - LeetPush

---
+++ Maximum-Subarray.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int MaxSubSum=nums[0],CurrentSubSum=nums[0];
+        for(int i=1;i<nums.size();i++){
+            CurrentSubSum=max(nums[i],CurrentSubSum+nums[i]);
+            MaxSubSum=max(MaxSubSum,CurrentSubSum);
+        }
+        return MaxSubSum;
+    }
+};
