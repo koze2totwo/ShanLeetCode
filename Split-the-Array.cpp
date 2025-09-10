a2590b61e9e938e787daaa28ab0c28d65595a429
shan <112062753+koze2totwo@users.noreply.github.com>
2025-09-10 13:09:11 +0530

    [3046] [Time Beats: 100.00%] [Memory Beats: 64.96%] - LeetPush

--- Split-the-Array.cpp
+++ Split-the-Array.cpp
@@ -2,8 +2,10 @@ class Solution {
 public:
     bool isPossibleToSplit(vector<int>& nums) {
         map<int,int> mp;
+        
         for(auto x:nums){
             mp[x]++;
+            //it would be impossible to split the in two parts if there is an elememt with frequency more than 2
             if(mp[x]>2){
                 return false;
             }
